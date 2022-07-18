c82a4c0a10cd1d9a703ccc403f0dd06d5784f511
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-18 07:47:08 -0300

    stories props

--- src/components/Main/StoriesPosts/Stories.js
+++ src/components/Main/StoriesPosts/Stories.js
@@ -1,62 +1,30 @@
+function Story(props) {
+  return (
+    <div>
+      <a className="story" href={props.link} alt={`stories de ${props.alt}`}>
+          <img src="./assets/img/Stories-ring.svg"/>
+          <img src={props.img}/>
+      </a>
+      <p><h4>{props.userName}</h4></p>
+    </div>
+  );
+}
+
 export default function Stories() {
+  const stories = [
+    {link:"https://www.instagram.com/nerdbunker/", alt:"Nerd Bunker", img:"./assets/img/nerbunker-logo.jpg", userName:"nerdbunker"},
+    {link:"https://www.instagram.com/9gag/", alt:"Nine Gag", img:"./assets/img/9gag-logo.svg", userName:"9gag"},
+    {link:"https://www.instagram.com/flamengodadepressao/", alt:"Flamengo da Depressão", img:"./assets/img/Flamengo-logo.svg", userName:"flamengodadepressao"},
+    {link:"https://www.instagram.com/edifica.adm/", alt:"Edifica admnistradora de Condomínios", img:"./assets/img/Edifica-logo.png", userName:"edifica.adm"},
+    {link:"https://www.instagram.com/respondeai/", alt:"Responde Aí", img:"./assets/img/Responde-ai.jpeg", userName:"respondeai"},
+    {link:"https://www.instagram.com/meowed/", alt:"Meowed", img:"./assets/img/meowed-logo.jpeg", userName:"meowed"},
+    {link:"https://www.instagram.com/barked/", alt:"Barked", img:"./assets/img/Barked-logo.jpeg", userName:"barked"},
+    {link:"https://www.instagram.com/oatila/", alt:"Átila Iamarino", img:"./assets/img/atila-foto.png", userName:"oatila"}
+  ];
+  
   return (
     <div className="stories">
-      <div>
-          <a className="story" href="https://www.instagram.com/nerdbunker/" alt="stories de Nerd bunker">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/nerbunker-logo.jpg"/>
-          </a>
-          <p><h4>nerdbunker</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/9gag/" alt="stories de Nine Gag">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/9gag-logo.svg"/>
-          </a>
-          <p><h4>9gag</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/flamengodadepressao/" alt="stories de Flamengo da Depressão">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/Flamengo-logo.svg"/>
-          </a>
-          <p><h4>flamengodadepressao</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/edifica.adm/" alt="stories de Edifica admnistradora de Condomínios">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/Edifica-logo.png"/>
-          </a>
-          <p><h4>edifica.adm</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/respondeai/" alt="stories de Responde Aí">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/Responde-ai.jpeg"/>
-          </a>
-          <p><h4>respondeai</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/meowed/" alt="stories de Meowed">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/meowed-logo.jpeg"/>
-          </a>
-          <p><h4>meowed</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/barked/" alt="stories de Barked">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/Barked-logo.jpeg"/>
-          </a>
-          <p><h4>barked</h4></p>
-      </div>
-      <div>
-          <a className="story" href="https://www.instagram.com/oatila/" alt="stories de Átila Iamarino">
-              <img src="./assets/img/Stories-ring.svg"/>
-              <img src="./assets/img/atila-foto.png"/>
-          </a>
-          <p><h4>oatila</h4></p>
-      </div>
+      {stories.map(story => <Story link={story.link} alt={story.alt} img={story.img} userName={story.userName}/>)}
       <a href=""><ion-icon name="chevron-forward-circle"></ion-icon></a>
     </div>
   );
