1ba253dc7ff97ed3ea667d1b457b50ddabd116c2
ZeGuilhermeYung <jgryung@gmail.com>
2022-07-17 23:30:00 -0300

    correções SideBar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 import StoriesPosts from "./Main/StoriesPosts.js";
 import SideBar from "./Main/SideBar.js";
-import OptionsConfigs from "./Main/OptionsConfigs";
 
 export default function Main() {
   return (
     <main>
       <StoriesPosts />
       <SideBar />
-      <OptionsConfigs />
     </main>
   );
 }
